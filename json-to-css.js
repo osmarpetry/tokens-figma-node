114127a53f2b7c4e9cd1949183394d5ba09fbcd3
osmarpetry <osmar.petry-ext@rio.cloud>
2020-09-20 14:43:54 -0300

    tá xodozinho

--- json-to-css.js
+++ json-to-css.js
@@ -3,54 +3,88 @@ const jsonToCssVariables = require('json-to-css-variables');
 
 var obj = JSON.parse(fs.readFileSync('tokens.json', 'utf8'));
 
-const teste = (obj) => {
-  console.log('=>', obj);
-  Object.entries(obj[1]).map((b) => {
-    setTimeout(function () {
-      teste(b);
-    }, 0);
+const fonts = (fontName, fonts) => {
+  const fontsReturn = [];
+  Object.entries(fonts).map((variableName) => {
+    Object.entries(variableName[1]).map((variableObject) => {
+
+      if (variableObject[0] !== 'type' && variableObject[0] !== 'value') {
+        const value =
+          fontName +
+          '-' +
+          variableName[0] +
+          '-' +
+          variableObject[0] +
+          '-value: "' +
+          variableObject[1].value +
+          '";';
+
+        const type =
+          fontName +
+          '-' +
+          variableName[0] +
+          '-' +
+          variableObject[0] +
+          '-type: "' +
+          variableObject[1].type +
+          '";';
+
+        fontsReturn.push(value, type);
+      } else {
+        console.log(variableObject[0], variableObject[1])
+
+        const value =
+          fontName +
+          '-' +
+          variableName[0] +
+          '-' +
+          variableObject[0] +
+          ': "' +
+          variableObject[1] +
+          '";';
+
+
+        fontsReturn.push(value);
+      }
+    });
   });
+
+  return fontsReturn;
 };
 
-const ka = Object.entries(obj.token).map((a) => {
-  let pa = [];
+fs.unlinkSync('tokens.css');
+var stream = fs.createWriteStream('tokens.css');
+stream.once('open', function (fd) {
+  stream.write('body {' + '\n');
 
-  Object.entries(a[1]).map((b) => {
-    let variable = `--${a[0]}`;
-    Object.entries(a[1]).map((b) => {
-      variable = variable + `-${b[0]}`;
-      Object.entries(b[1]).map((c) => {
-        c.map((d, k) => {
-          if (typeof d == 'string') {
-            if (k % 2 === 0) {
-              pa.push(variable + `-${d}`);
-            }
+  Object.entries(obj.token).map((variableName) => {
+    Object.entries(variableName[1]).map((b) => {
+      let variables = [];
+      b.map((variableSubName, index) => {
+        if (index % 2 === 0) {
+          variables.push(`   --${variableName[0]}-${variableSubName}`);
+        } else {
+          if (variableSubName.value) {
+            variables[1] =
+              variables[0] + '-value: "' + variableSubName.value + '";';
+            variables[2] =
+              variables[0] + '-type: "' + variableSubName.type + '";';
           } else {
-            if (k % 2 === 1) {
-              pa[pa.length - 1] = pa[pa.length - 1] + `: ${d.value};`;
-            }
+            const font = fonts(variables[0], variableSubName);
+            font.map((fontVariable, index) => {
+              if (index % 2 == 0) {
+                variables[1] = fontVariable;
+              } else {
+                variables[2] = fontVariable;
+              }
+            });
           }
-        });
+        }
       });
+      stream.write(variables[1] + '\n');
+      stream.write(variables[2] + '\n');
     });
   });
 
-  return pa;
-});
-
-fs.unlinkSync('tokens.css')
-
-var stream = fs.createWriteStream('tokens.css', { flags: 'a' });
-ka.map((ppak) => {
-  stream.once('open', function (fd) {
-    stream.write('body {' + '\n');
-  });
-  ppak.map((papak) => {
-    stream.once('open', function (fd) {
-      stream.write('   ' + papak + '\n');
-    });
-  });
-  stream.once('open', function (fd) {
-    stream.write('}' + '\n');
-  });
+  stream.write('}');
 });
